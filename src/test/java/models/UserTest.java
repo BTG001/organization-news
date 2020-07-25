7b32292ad4db13475aa10a0ab542d26fff5f429c
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 08:01:03 +0300

    Add necessary test for the Users.java class

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,4 +1,59 @@
 package models;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class UserTest {
+
+    @Test
+    public void getNamereturnsName() throws Exception {
+        Users testUsers= setupusers();
+        assertEquals("Otieno", testUsers.getName());
+    }
+
+    @Test
+    public void setName() {
+        Users testUsers= setupusers();
+        testUsers.setName("Otieno");
+        assertNotEquals("Okello", testUsers.getName());
+    }
+
+    @Test
+    public void getRoleReturnsRole() {
+        Users testUsers= setupusers();
+        assertEquals("handle finance business", testUsers.getRole());
+    }
+
+    @Test
+    public void setRole() {
+        Users testUsers= setupusers();
+        testUsers.setRole("handle finance business");
+        assertNotEquals("handle accounting business", testUsers.getRole());
+    }
+
+    @Test
+    public void getPositionReturnsPosition() {
+        Users testUsers= setupusers();
+        assertEquals("officer", testUsers.getPosition());
+    }
+
+    @Test
+    public void setPosition() {
+        Users testUsers= setupusers();
+        testUsers.setPosition("officer");
+        assertNotEquals("manager", testUsers.getPosition());
+    }
+
+    @Test
+    public void getIdReturnsId() {
+
+    }
+
+    @Test
+    public void setId() {
+    }
+
+
 }
