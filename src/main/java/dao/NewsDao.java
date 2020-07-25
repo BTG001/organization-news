0d0ac5ba6a2238a87e8c67acaef313ae4af2a3df
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 08:26:32 +0300

    Update the NewsDao interface

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Department;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+    void addNewsToDepartment(News news, Department department);
+
+    //read
+    List<News> getAll();
+    List<Department> getAllDepartmentsForANews(int id);
+    News findById(int id);
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
