b65f363d629632dd9040aca01bdb8619f9f3bb4b
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 09:17:53 +0300

    connect to postgres database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,52 @@
+import static spark.Spark.*;
+
+import dao.DepartmentDao;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import models.DB;
+import models.Department;
+import models.News;
+import models.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import com.google.gson.Gson;
+import exceptions.ApiException;
+import spark.ModelAndView;
+import org.sql2o.Connection;
+import spark.ModelAndView;
+import spark.template.handlebars.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        DepartmentDao departmentDao;
+        Sql2oUsersDao usersDao;
+        Sql2oNewsDao newsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:postgresql://localhost:5432/news_api";
+        Sql2o sql2o = new Sql2o(connectionString, "buno", "Bruno11057");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
+
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        },new HandlebarsTemplateEngine());
+
+    }
 }
